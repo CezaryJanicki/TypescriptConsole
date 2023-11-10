131d2911d9f31203d996b9ad66f1f244b3078948
CezaryJanicki <cezary.janicki@gmail.com>
2023-11-10 12:49:12 +0100

    Add UsersData

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 const consola = require('consola');
 const inquirer = require('inquirer');
 
-
 enum Action {
   List = "list",
   Add = "add",
@@ -13,20 +12,17 @@ type InquirerAnswers = {
   action: Action
 }
 
-//create enum for consola success, error and info
 enum MessageVariant {
   Success = "success",
   Error = "error",
   Info = "info"
 }
 
-//create Message class
 class Message {
   constructor(public content: string) { }
 
   public show() {
-    //log contenst of text
-    console.log(this.content);
+      console.log(this.content);
   }
   public capitalize() {
     this.content = this.content.toUpperCase();
@@ -47,19 +43,56 @@ class Message {
     }
   }
 }
-  const msg = new Message("heLlo world!");
-  msg.show(); // "heLlo world!"
-  msg.capitalize();
-  msg.show(); // "Hello world!"
-  msg.toLowerCase();
-  msg.show(); // "hello world!"
-  msg.toUpperCase();
-  msg.show(); // "HELLO WORLD!"
-  Message.showColorized(MessageVariant.Success, "Test"); // √ "Test"
-  Message.showColorized(MessageVariant.Error, "Test 2"); // "x Test 2"
-  Message.showColorized(MessageVariant.Info, "Test 3"); // ℹ "Test 3"
 
-//add async before collback function
+interface User {
+  name: string;
+  age: number;
+}
+
+class UsersData {
+  data: User[] = [];
+
+  showAll() {
+    Message.showColorized(MessageVariant.Info, "Users data");
+    if (this.data.length === 0) {
+      Message.showColorized(MessageVariant.Info, "No data...");
+    } else {
+      console.table(this.data);
+    }
+  }
+
+  public add(user: User) {
+    if (typeof user.name === "string" && user.name.length > 0 && typeof user.age === "number" && user.age > 0) {
+      this.data.push(user);
+      Message.showColorized(MessageVariant.Success, "User has been successfully added!");
+    } else {
+      Message.showColorized(MessageVariant.Error, "Wrong data!");
+    }
+  }
+
+  public remove(name: string) {
+    const index = this.data.findIndex((user) => user.name === name);
+    if (index > -1) {
+      this.data.splice(index, 1);
+      Message.showColorized(MessageVariant.Success, "User deleted!");
+    } else {
+      Message.showColorized(MessageVariant.Error, "User not found...");
+    }
+  }
+
+}
+
+const users = new UsersData();
+users.showAll();
+users.add({ name: "Jan", age: 20 });
+users.add({ name: "Adam", age: 30 });
+users.add({ name: "Kasia", age: 23 });
+users.add({ name: "Basia", age: -6 });
+users.showAll();
+users.remove("Maurycy");
+users.remove("Adam");
+users.showAll();
+
 const startApp = async () => {
   const answers: InquirerAnswers = await inquirer.prompt([{
     name: 'action',
